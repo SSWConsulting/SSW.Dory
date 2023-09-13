d97b99c885552346bad824c86883945d0b4990bf
Jord <JordGui@ssw.com.au>
2023-09-13 15:18:17 +1000

    Style nav bar

--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <nav>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <nav className="bggrey w-full h-24 p-4 flex justify-center">
         <Image
           src="/sswdory-logo.png"
           alt="Next.js Logo"
-          width={180}
-          height={37}
+          width={300}
+          height={40}
           priority
         />
       </nav>
