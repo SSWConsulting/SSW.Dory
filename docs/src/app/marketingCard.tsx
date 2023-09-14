b4eb55e37e1d8daf231432a5b24faef15e38468b
Jord <JordGui@ssw.com.au>
2023-09-14 10:52:48 +1000

    create marketingCard component

---
+++ docs/src/app/marketingCard.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+type MarketingCardProps = {
+  title: string;
+  description: string;
+  imgURL: string;
+};
+
+export default function MarketingCard({
+  title,
+  description,
+  imgURL,
+}: MarketingCardProps) {
+  return (
+    <section className="text-gray-700 body-font">
+      <div className="container mx-auto flex px-5 py-19 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+          <h1 className="title-font leading-tight sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            {title}
+          </h1>
+          <p className="mb-8 leading-relaxed">{description}</p>
+        </div>
+        <div className="md:w-1/2 w-5/6 mb-10 md:mb-0">
+          <a href={imgURL} target="_blank">
+            <Image
+              className="object-cover object-center rounded bordered"
+              alt="hero"
+              src={imgURL}
+              width={620}
+              height={400}
+            />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import MarketingCard from "./marketingCard";
 
 export default function Home() {
   return (
@@ -14,18 +15,13 @@ export default function Home() {
       </nav>
       <div className="container mx-auto">
         <section className="text-gray-700 body-font">
-          <div
-            className="container mx-auto flex px-5 py-20 items-center justify-center flex-col"
-          >
+          <div className="container mx-auto flex px-5 py-20 items-center justify-center flex-col">
             <div className="text-center lg:w-2/3 w-full">
               <h1 className="title-font leading-tight sm:text-4xl text-3xl mb-10 font-medium text-gray-900">
                 Check out our Github
               </h1>
             </div>
-            <a
-              href="https://github.com/SSWConsulting/SSW.Dory"
-              target="_blank"
-            >
+            <a href="https://github.com/SSWConsulting/SSW.Dory" target="_blank">
               <Image
                 src="/github-mark.svg"
                 alt="Invertocat Logo"
@@ -36,6 +32,12 @@ export default function Home() {
             </a>
           </div>
         </section>
+
+        <MarketingCard
+          title="Powered by Microsoft Power Automate"
+          description="A low-code solution for easy set-up"
+          imgURL="/dory-architecture-diagram.svg"
+        ></MarketingCard>
       </div>
     </main>
   );
