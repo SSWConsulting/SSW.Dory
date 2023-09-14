183b6d2c9e1abf64269be41dd8f7d4ab818e06c6
Jord <JordGui@ssw.com.au>
2023-09-14 12:21:45 +1000

    create MarketingCardText component

--- docs/src/app/marketingCard.tsx
+++ docs/src/app/marketingCard.tsx
@@ -1,11 +1,27 @@
 import Image from "next/image";
 
+type MarketingCardTextProps = {
+  title: string;
+  description: string;
+};
+
 type MarketingCardProps = {
   title: string;
   description: string;
   imgURL: string;
   reverse?: boolean;
 };
+
+function MarketingCardText({ title, description }: MarketingCardTextProps) {
+  return (
+    <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+      <h1 className="title-font leading-tight sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+        {title}
+      </h1>
+      <p className="mb-8 leading-relaxed">{description}</p>
+    </div>
+  );
+}
 
 export default function MarketingCard({
   title,
@@ -17,12 +33,7 @@ export default function MarketingCard({
     <section className="text-gray-700 body-font">
       <div className="container mx-auto flex px-5 py-10 md:flex-row flex-col items-center">
         {!reverse && (
-          <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-            <h1 className="title-font leading-tight sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              {title}
-            </h1>
-            <p className="mb-8 leading-relaxed">{description}</p>
-          </div>
+          <MarketingCardText title={title} description={description} />
         )}
         <div className="md:w-1/2 w-5/6 mb-10 md:mb-0">
           <a href={imgURL} target="_blank">
@@ -36,12 +47,7 @@ export default function MarketingCard({
           </a>
         </div>
         {reverse && (
-          <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-            <h1 className="title-font leading-tight sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              {title}
-            </h1>
-            <p className="mb-8 leading-relaxed">{description}</p>
-          </div>
+          <MarketingCardText title={title} description={description} />
         )}
       </div>
     </section>
