6731be8e4b2a267ca42a8011bb4c27a6f6e0c517
Jord <JordGui@ssw.com.au>
2023-09-14 11:24:16 +1000

    fix padding y

--- docs/src/app/marketingCard.tsx
+++ docs/src/app/marketingCard.tsx
@@ -15,7 +15,7 @@ export default function MarketingCard({
 }: MarketingCardProps) {
   return (
     <section className="text-gray-700 body-font">
-      <div className="container mx-auto flex px-5 py-19 md:flex-row flex-col items-center">
+      <div className="container mx-auto flex px-5 py-10 md:flex-row flex-col items-center">
         {!reverse && (
           <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
             <h1 className="title-font leading-tight sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
