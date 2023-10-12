75b22c3d9fd58c6e2fc1ca182bb4a0bd0cb9aabb
Jord <JordGui@ssw.com.au>
2023-10-12 16:12:52 +1100

    reduce to one h1 tag, convert remaining to h2 and p to h3 for seo

--- docs/src/app/marketingCard.tsx
+++ docs/src/app/marketingCard.tsx
@@ -15,10 +15,10 @@ type MarketingCardProps = {
 function MarketingCardText({ title, description }: MarketingCardTextProps) {
   return (
     <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-      <h1 className="title-font leading-tight sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+      <h2 className="title-font leading-tight sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
         {title}
-      </h1>
-      <p className="mb-8 leading-relaxed max-w-xl">{description}</p>
+      </h2>
+      <h3 className="mb-8 leading-relaxed max-w-xl">{description}</h3>
     </div>
   );
 }
--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <section className="text-gray-700 body-font">
           <div className="container mx-auto flex px-5 py-20 items-center justify-center flex-col">
             <div className="text-center lg:w-2/3 w-full">
-              <h1 className="title-font leading-tight sm:text-4xl text-3xl mb-10 font-medium text-gray-900">
+              <h1 className="title-font leading-tight sm:text-5xl text-4xl mb-10 font-semibold text-gray-900">
                 Check out our Github
               </h1>
             </div>
