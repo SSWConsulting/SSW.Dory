e0fd29d13544388833cf38bff0fedd3c0195e6ff
Jord Gui [SSW] <41951199+Jord-Gui@users.noreply.github.com>
2023-11-21 09:46:31 +0700

    Swap header and tagline, edit headings

--- content/src/app/page.tsx
+++ content/src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
           <div className="container mx-auto mb-32 md:mb-0 flex px-5 py-10 items-center justify-center flex-col">
             <div className="text-center lg:w-2/3 w-full">
               <h1 className="title-font leading-tight sm:text-5xl text-4xl mb-10 font-semibold text-gray-900">
-                This project is on GitHub
+                Be informed before someone tells you they&apos;re blocked
               </h1>
             </div>
             <a href="https://github.com/SSWConsulting/SSW.Dory" target="_blank">
@@ -35,33 +35,33 @@ export default function Home() {
             </a>
             <div className="lg:flex-grow flex flex-col md:items-start md:text-left items-center text-center">
               <h3 className="leading-relaxed max-w-xl mt-10">
-                Be informed before someone tells you they&apos;re blocked.
+                This project is on GitHub.
               </h3>
             </div>
           </div>
         </section>
 
         <MarketingCard
-          title="Discover checked-out SharePoint pages"
+          title="Remind users of their checked-out SharePoint pages"
           description="Connect SSW Dory to your SharePoint site and find any pages that have been left checked-out."
           imgURL="/old-sharepoint.png"
         />
 
         <MarketingCard
-          title="Identify unmerged GitHub pull requests"
+          title="Remind users of their unmerged GitHub pull requests"
           description="SSW Dory uses the GitHub API to scan for any Pull Requests in your GitHub repositories that have been open for longer than a certain period of time e.g. one day."
           imgURL="/old-github.png"
           reverse={true}
         />
 
         <MarketingCard
-          title="Friendly reminder emails"
+          title="Friendly reminder emails for users"
           description="SSW Dory will send out emails to the relevent people who have left a SharePoint page checked-out or a GitHub PR open for too long."
           imgURL="/email-github.png"
         />
 
         <MarketingCard
-          title="Piece of mind from a portal"
+          title="Piece of mind from a portal for managers"
           description="The SharePoint site shows you the current offenders."
           imgURL="/sharepoint-list.png"
           reverse={true}
